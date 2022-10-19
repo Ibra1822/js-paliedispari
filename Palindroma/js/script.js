5baa8162875f62faf6e99069e34b19f4e66ca34f
“Ibra1822” <ibrahimmujagic18@gmail.com>
2022-10-19 22:27:08 +0200

    fix all

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,22 +1,56 @@
+
+
+let btn = document.getElementById('btn');
+
+let testo = document.getElementById('testo')
+
+let h1 = document.createElement('h1')
+
+h1.innerHTML=`Inserisci qua sotto una parola palindroma è il pc ti dara la conferma se lo è`;
+
+let title = document.querySelector('.container-title')
+
+title.append(h1)
+
+let ris= document.querySelector('.ris');
+
+
+btn.addEventListener('click', function(){
+  
   // chiedo all'utente di inserire una parola, e salvo il valore
-   let parolaInserita = prompt('inserisci una parola palindroma').toLowerCase()
-
-//dichiaro la funzione di nome isPalindroma, che prende come argomento una parola
-
- function isPalindroma (checkPali){
- let parolaInvertita = ''
-
- for (let i =  checkPali.length - 1; i >= 0; i-- ){
- parolaInvertita += checkPali[i];
+  let parolaInserita = testo.value;
   
-  }
-  return parolaInvertita;
+  // //dichiaro la funzione di nome isPalindroma, che prende come argomento una parola
+  function isPalindroma (checkPali){
+    let parolaInvertita = ''
+   
+    for (let i =  checkPali.length - 1; i >= 0; i-- ){
+    parolaInvertita += checkPali[i];  
+     
+     }
+     return parolaInvertita;
+     
+   }  
+     
+   if(parolaInserita === isPalindroma(parolaInserita)) {
   
-}
+    ris.innerHTML =` 
+    La parola che hai inserito <strong> ${parolaInserita} </strong> è palindroma
+    
+    `
+  } else {
+
+    ris.innerHTML =` 
+    La parola che hai inserito <strong> ${parolaInserita} </strong> non è palindroma
+    
+    `
+   }  
   
-if(parolaInserita === isPalindroma(parolaInserita)) {
-  console.log('Parola palindroma')
-} else {
-  console.log('Parola non palindroma')
-}
+  
+  
+  
+  
+})   
+
+
 
--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -2,13 +2,13 @@ let scelta = prompt('Scegli pari o dispari').toLocaleLowerCase();
 
 let sceltaN = parseInt(prompt('Inserisci un numero da 1 a 5'));
 
-let pcRandom =  Math.ceil(Math.random() * 5 );
+let pcRandom =  casuale()
 
 let somma = calcolo(sceltaN , pcRandom);
 
 let conclusione = fine();
 
-console.log('conclusione',conclusione);
+console.log('Quindi',conclusione);
 
 console.log('somma',somma);
 
@@ -18,12 +18,35 @@ console.log('scelta', scelta );
 
 console.log('scelta',sceltaN);
 
+let output = document.querySelector('.container')
+
+output.prepend(`Hai scelto  ${scelta} e il numero che hai scelto è ${sceltaN} mentre il tuo pc ha scelto è ${pcRandom}  `)
+
+let h1 = document.createElement('h1')
+
+
+h1.innerHTML= conclusione;
+
+output.append(h1)
+
+
+let btn = document.getElementById('btn');
+
+
+btn.addEventListener('click', function(){location.reload()});
 
 /**************************************************/
 /**************************************************/
 /**************************************************/
 /**************************************************/
 
+
+
+function casuale (){
+
+   return Math.ceil(Math.random() * 5 )
+
+}
 
 function calcolo (n1,n2) {
 
@@ -37,22 +60,22 @@ function fine() {
   
   if(somma % 2 === 0 && scelta === 'pari' ){
     
-    return 'Hai vinto è pari e il risultato è pari Complimenti '
+    return 'Hai vinto la tua scelta è giusta il risultato è pari Complimenti '
 
   }else if(somma % 2 === 0 && scelta === 'dispari'){
   
-    return 'Hai scelto dispari ma il risultato è pari hai Perso'
+    return 'Hai scelto dispari ma il risultato è pari ci dispiace ma hai Perso'
   
   }else if(somma % 2 !== 0 && scelta === 'pari'){
 
-    return 'Hai scelto pari ma il risultato è dispari hai Perso'
+    return 'Hai scelto pari ma il risultato è dispari ci dispiace ma hai Perso'
   
   }else if(somma % 2 !== 0 && scelta === 'dispari'){
 
     return 'Hai scelto dispari ed hai vinto Complimenti'
   
   }else{
-    return 'devi dire pari o dispari'
+    return 'devi dire pari o dispari e scegliere un numero!'
   }
 
 }
